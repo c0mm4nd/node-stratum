8f96af7574246b0fbbedca9c167d22af771bad88
Command <maoxs2@163.com>
2020-01-03 02:58:23 +0800

    update index

--- index.js
+++ index.js
@@ -1,54 +1,7 @@
-const myCoin = {
-    "name": "Litecoin",
-    "symbol": "LTC",
-    "algorithm": "scrypt",
-};
-
-
 const Pool = require("./lib/pool");
-const pool = new Pool({
-    "coin": myCoin,
-    "address": "Qbyk864gpbz46XQqgf6DKKqXED9KtfxD2E",
-    "rewardRecipients": {},
+const config = require("./config");
 
-    "blockRefreshInterval": 1000,
-    "jobRebroadcastTimeout": 55,
-
-    "connectionTimeout": 600,
-
-    "emitInvalidBlockHashes": false,
-    "tcpProxyProtocol": false,
-    "banning": {
-        "enabled": true,
-        "time": 600,
-        "invalidPercent": 50,
-        "checkThreshold": 500,
-        "purgeInterval": 300
-    },
-
-    "ports": {
-        "3032": {
-            "diff": 8,
-            "varDiff": {
-                "minDiff": 1,
-                "maxDiff": 512,
-                "targetTime": 15,
-                "retargetTime": 90,
-                "variancePercent": 30
-            }
-        }
-    },
-
-    "daemons": [
-        {
-            "host": "127.0.0.1",
-            "port": 19332,
-            "user": "litecoinrpc",
-            "password": "testnet"
-        }
-    ],
-
-}, (ip, port , workerName, password, callback) => {
+const pool = new Pool(config, (ip, port , workerName, password, callback) => {
     console.log("Authorize " + workerName + ":" + password + "@" + ip);
     callback({
         error: null,
